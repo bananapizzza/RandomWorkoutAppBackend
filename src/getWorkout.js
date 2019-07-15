68ddf90dacc5f42d1ae7e3652b9f993f5b235d81
Minseung Kwon <bananapizzza@gmail.com>
2019-07-15 12:33:45 -0700

    Changed the data path using root path.

--- src/getWorkout.js
+++ src/getWorkout.js
@@ -2,9 +2,11 @@ const express = require('express');
 const port = 5000;
 const fs = require('fs');
 const app = express();
+const path = require('path');
+const appRoot = path.resolve(__dirname);
 
 app.get('/', (req, res) => {
-    fs.readFile('../data/workout_list', 'utf-8', (err, workoutList) => {
+    fs.readFile(`${path.parse(appRoot).dir}/data/workout_list`, 'utf-8', (err, workoutList) => {
         res.append('Access-Control-Allow-Origin', ['*']);
         res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.append('Access-Control-Allow-Headers', 'Content-Type');
