048634dc8bba6048841c18fb9b6b3ae59359cfed
Minseung Kwon <bananapizzza@gmail.com>
2019-07-15 11:38:59 -0700

    Changed to use Express instead of http.

--- src/getWorkout.js
+++ src/getWorkout.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const port = 5000;
-const http = require('http');
 const fs = require('fs');
 const app = express();
 
-const httpServer = http.createServer((req, res) => {
+app.get('/', (req, res) => {
     fs.readFile('../data/workout_list', 'utf-8', (err, workoutList) => {
-        if(!err){
-            res.writeHead(200, {'Access-Control-Allow-Origin':'*'});
-            res.end(JSON.stringify(workoutList));
-        } else {
-            res.writeHead(404, {'Access-Control-Allow-Origin':'*'});
-            res.end(JSON.stringify(err));
-        }
+        res.append('Access-Control-Allow-Origin', ['*']);
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        res.send(JSON.stringify(workoutList));
     });
 });
 
-httpServer.listen(port);
-
-//TODO: Change to use express
-
-// app.get('/', (req, res) => {
-//     fs.readFile('../data/workout_list', 'utf-8', (err, workoutList) => {
-//         res.send(workoutList);
-//         console.log(workoutList);
-//     });
-// });
-//
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
