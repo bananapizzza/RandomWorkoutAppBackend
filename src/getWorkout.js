68fd513afaaf84ffb79184468e05cb4f1c4d7d26
Minseung Kwon <bananapizzza@gmail.com>
2019-07-15 12:37:11 -0700

    Changed Access-Control-Allow-Methods from 'GET,PUT,POST,DELETE' to 'GET'

--- src/getWorkout.js
+++ src/getWorkout.js
@@ -8,7 +8,7 @@ const appRoot = path.resolve(__dirname);
 app.get('/', (req, res) => {
     fs.readFile(`${path.parse(appRoot).dir}/data/workout_list`, 'utf-8', (err, workoutList) => {
         res.append('Access-Control-Allow-Origin', ['*']);
-        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Methods', 'GET');
         res.append('Access-Control-Allow-Headers', 'Content-Type');
         res.send(JSON.stringify(workoutList));
     });
