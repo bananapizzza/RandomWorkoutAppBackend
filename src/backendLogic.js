7debd60f7da9b95b438f90dea78f727acd93ea2e
Minseung Kwon <bananapizzza@gmail.com>
2019-07-22 17:30:13 -0700

    Added codes for getting login data from frontend server.

--- src/backendLogic.js
+++ src/backendLogic.js
@@ -4,7 +4,11 @@ const fs = require('fs');
 const app = express();
 const path = require('path');
 const appRoot = path.resolve(__dirname);
+const cors = require('cors');
 let workoutList;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     fs.readFile(`${path.parse(appRoot).dir}/data/workout_list.json`, 'utf-8', (err, data) => {
@@ -13,13 +17,14 @@ app.get('/', (req, res) => {
 
         //Select workout randomly from the array
         const selectedWorkout = workoutList[Math.floor(Math.random() * workoutList.length)];
-
-        //Send the selected workout data
-        res.append('Access-Control-Allow-Origin', ['*']);
-        res.append('Access-Control-Allow-Methods', 'GET');
-        res.append('Access-Control-Allow-Headers', 'Content-Type');
         res.send(JSON.stringify(selectedWorkout));
     });
+});
+
+app.post('/check_login_info', (req, res) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    res.send(JSON.stringify("hello"));
 });
 
 app.listen(port, () => {
