1998c813252024b373e2f8602d055d270eeac82d
Minseung Kwon <bananapizzza@gmail.com>
2019-07-25 16:15:47 -0700

    Changed callback functions of get and post to async function.

--- src/backendLogic.js
+++ src/backendLogic.js
@@ -13,9 +13,9 @@ const rootPath = path.parse(appRoot).dir;
 app.use(express.json());
 app.use(cors());
 
-async function getFileContent(filePath){
+async function getFileContent(filePath) {
     let fileContent;
-    try{
+    try {
         fileContent = await readFileAsync(filePath, 'utf-8');
         return fileContent;
     } catch (err) {
@@ -24,37 +24,38 @@ async function getFileContent(filePath){
 }
 
 //For selecting workout randomly
-app.get('/', (req, res) => {
-    getFileContent(`${rootPath}/data/workout_list.json`).then(fileContent => {
-        const workoutList = JSON.parse(fileContent).workouts;
+app.get('/', async (req, res) => {
+    const fileContent = await getFileContent(`${rootPath}/data/workout_list.json`);
+    const workoutList = JSON.parse(fileContent).workouts;
 
-        //Select workout randomly from the array
-        const selectedWorkout = workoutList[Math.floor(Math.random() * workoutList.length)];
-        res.send(JSON.stringify(selectedWorkout));
-    });
+    //Select workout randomly from the array
+    const selectedWorkout = workoutList[Math.floor(Math.random() * workoutList.length)];
+    res.send(JSON.stringify(selectedWorkout));
 });
 
+//TODO: After making logic for sign up, use crypto to encrypt password
 //For checking login info
-app.post('/check_login_info', (req, res) => {
-    getFileContent(`${rootPath}/data/user_list.json`).then(fileContent => {
-        const userList = JSON.parse(fileContent).users;
-        let isValidUser = false;
-        for(i=0; i<userList.length; i++){
-            if(userList[i].username === req.body.username){
-                isValidUser = true;
-                if(userList[i].password === req.body.password) {
-                    res.send(JSON.stringify("Login Success"));
-                } else {
-                    res.statusCode = 401;
-                    res.send(JSON.stringify("Wrong Password"));
-                }
+app.post('/check_login_info', async (req, res) => {
+    const fileContent = await getFileContent(`${rootPath}/data/user_list.json`);
+    const userList = JSON.parse(fileContent).users;
+    let isValidUser = false;
+    for (i = 0; i < userList.length; i++) {
+        if (userList[i].username === req.body.username) {
+            isValidUser = true;
+            if (userList[i].password === req.body.password) {
+                res.send(JSON.stringify("Login Success"));
+            } else {
+                res.statusCode = 401;
+                res.send(JSON.stringify("Wrong Password"));
             }
         }
-        if(!isValidUser){
-            res.statusCode = 400;
-            res.send(JSON.stringify("Invalid User"));
-        }
-    });
+    }
+    if (!isValidUser) {
+        res.statusCode = 400;
+        res.send(JSON.stringify("Invalid User"));
+    }
+
+
 });
 
 app.listen(port, () => {
