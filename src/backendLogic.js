ef657416b885efe96fab7fbf4373aa181235505e
Minseung Kwon <bananapizzza@gmail.com>
2019-07-29 13:37:07 -0700

    Changed structure of checking login info logic. (Made a new func named checkLoginInfo)

--- src/backendLogic.js
+++ src/backendLogic.js
@@ -7,6 +7,9 @@ const cors = require('cors');
 const util = require('util');
 
 const port = 5000;
+const INVALID_USER = 'INVALID_USER';
+const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+const WRONG_PASSWORD = 'WRONG_PASSWORD';
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 const rootPath = path.parse(appRoot).dir;
@@ -14,15 +17,7 @@ const rootPath = path.parse(appRoot).dir;
 app.use(express.json());
 app.use(cors());
 
-async function getFileContent(filePath) {
-    let fileContent;
-    try {
-        fileContent = await readFileAsync(filePath, 'utf-8');
-        return fileContent;
-    } catch (err) {
-        console.log('error: ', err);
-    }
-}
+
 
 //For selecting workout randomly
 app.get('/', async (req, res) => {
@@ -39,21 +34,19 @@ app.get('/', async (req, res) => {
 app.post('/check_login_info', async (req, res) => {
     const fileContent = await getFileContent(`${rootPath}/data/user_list.json`);
     const userList = JSON.parse(fileContent).users;
-    let isValidUser = false;
-    for (i = 0; i < userList.length; i++) {
-        if (userList[i].username === req.body.username) {
-            isValidUser = true;
-            if (userList[i].password === req.body.password) {
-                res.send(JSON.stringify("Login Success"));
-            } else {
-                res.statusCode = 401;
-                res.send(JSON.stringify("Wrong Password"));
-            }
-        }
-    }
-    if (!isValidUser) {
-        res.statusCode = 400;
-        res.send(JSON.stringify("Invalid User"));
+
+    switch(await checkLoginInfo(userList, req.body.username, req.body.password)){
+        case LOGIN_SUCCESS:
+            res.send(JSON.stringify("Login Success"));
+            break;
+        case WRONG_PASSWORD:
+            res.statusCode = 401;
+            res.send(JSON.stringify("Wrong Password"));
+            break;
+        case INVALID_USER:
+            res.statusCode = 400;
+            res.send(JSON.stringify("Invalid User"));
+            break;
     }
 });
 
@@ -71,3 +64,26 @@ app.post('/sign_up', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+async function getFileContent(filePath) {
+    let fileContent;
+    try {
+        fileContent = await readFileAsync(filePath, 'utf-8');
+        return fileContent;
+    } catch (err) {
+        console.log('error: ', err);
+    }
+}
+
+async function checkLoginInfo(userList, username, password){
+    for (i = 0; i < userList.length; i++) {
+        if (userList[i].username === username) {
+            if (userList[i].password === password) {
+                return LOGIN_SUCCESS;
+            } else {
+                return WRONG_PASSWORD;
+            }
+        }
+    }
+    return INVALID_USER;
+}
