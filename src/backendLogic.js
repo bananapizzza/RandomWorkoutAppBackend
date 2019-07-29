bdb7200255adf393b45510b8209599c9932c6ac5
Minseung Kwon <bananapizzza@gmail.com>
2019-07-29 13:52:07 -0700

    Added sign up checking logic.

--- src/backendLogic.js
+++ src/backendLogic.js
@@ -10,13 +10,15 @@ const port = 5000;
 const INVALID_USER = 'INVALID_USER';
 const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 const WRONG_PASSWORD = 'WRONG_PASSWORD';
+const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+const ALREADY_USED_ID = 'ALREADY_USED_ID';
+
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 const rootPath = path.parse(appRoot).dir;
 
 app.use(express.json());
 app.use(cors());
-
 
 
 //For selecting workout randomly
@@ -35,17 +37,17 @@ app.post('/check_login_info', async (req, res) => {
     const fileContent = await getFileContent(`${rootPath}/data/user_list.json`);
     const userList = JSON.parse(fileContent).users;
 
-    switch(await checkLoginInfo(userList, req.body.username, req.body.password)){
+    switch (await checkLoginInfo(userList, req.body.username, req.body.password)) {
         case LOGIN_SUCCESS:
-            res.send(JSON.stringify("Login Success"));
+            res.send(JSON.stringify(LOGIN_SUCCESS));
             break;
         case WRONG_PASSWORD:
             res.statusCode = 401;
-            res.send(JSON.stringify("Wrong Password"));
+            res.send(JSON.stringify(WRONG_PASSWORD));
             break;
         case INVALID_USER:
             res.statusCode = 400;
-            res.send(JSON.stringify("Invalid User"));
+            res.send(JSON.stringify(INVALID_USER));
             break;
     }
 });
@@ -54,11 +56,20 @@ app.post('/check_login_info', async (req, res) => {
 app.post('/sign_up', async (req, res) => {
     const fileContent = await getFileContent(`${rootPath}/data/user_list.json`);
     const jsonContent = JSON.parse(fileContent);
-    jsonContent['users'].push({username: req.body.username, password: req.body.password});
+    const userList = jsonContent.users;
 
-   await writeFileAsync(`${rootPath}/data/user_list.json`, JSON.stringify(jsonContent));
-
-    res.send(JSON.stringify("Sign Up Success"));
+    switch(await checkSignUpInfo(userList, req.body.username)) {
+        case SIGN_UP_SUCCESS:
+            //Add the new user to the json
+            jsonContent['users'].push({username: req.body.username, password: req.body.password});
+            await writeFileAsync(`${rootPath}/data/user_list.json`, JSON.stringify(jsonContent));
+            res.send(JSON.stringify(SIGN_UP_SUCCESS));
+            break;
+        case ALREADY_USED_ID:
+            res.statusCode = 402;
+            res.send(JSON.stringify(ALREADY_USED_ID));
+            break;
+    }
 });
 
 app.listen(port, () => {
@@ -75,7 +86,7 @@ async function getFileContent(filePath) {
     }
 }
 
-async function checkLoginInfo(userList, username, password){
+async function checkLoginInfo(userList, username, password) {
     for (i = 0; i < userList.length; i++) {
         if (userList[i].username === username) {
             if (userList[i].password === password) {
@@ -86,4 +97,13 @@ async function checkLoginInfo(userList, username, password){
         }
     }
     return INVALID_USER;
+}
+
+async function checkSignUpInfo(userList, username) {
+    for (i = 0; i < userList.length; i++) {
+        if (userList[i].username === username) {
+            return ALREADY_USED_ID;
+        }
+    }
+    return SIGN_UP_SUCCESS;
 }
